5cb0b86a983057ebab58ee8ba0bf0a90f9f7597a
momo <thangmo_thangmo@hotmail.com>
2015-06-27 17:22:18 +0700

    add account

---
+++ src/main/java/th/co/geniustree/intenship/advisor/controller/AccountController.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package th.co.geniustree.intenship.advisor.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import th.co.geniustree.intenship.advisor.model.Account;
+import th.co.geniustree.intenship.advisor.repo.AccountRepo;
+
+/**
+ *
+ * @author User
+ */
+@RestController
+public class AccountController {
+    @Autowired
+    private AccountRepo accountRepo;
+    
+    @RequestMapping(value = "/account",method = RequestMethod.GET)
+    public Page<Account> getAccount(Pageable pageable){
+        return accountRepo.findAll(pageable);
+    }
+    
+    @RequestMapping(value = "/account",method = RequestMethod.POST)
+    public void saveAccount(@Validated @RequestBody Account account){
+        accountRepo.save(account);
+    }
+    
+    @RequestMapping(value = "/accounts",method = RequestMethod.POST)
+    public void deleteAccount(Account account){
+        accountRepo.delete(account);
+    }
+}
---
+++ src/main/java/th/co/geniustree/intenship/advisor/model/Account.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package th.co.geniustree.intenship.advisor.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author User
+ */
+@Entity
+public class Account implements Serializable{
+    @Id
+    private Integer id;
+    private String password;
+    private String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 71 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Account other = (Account) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+    
+}
