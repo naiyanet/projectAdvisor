0999f99a7b5f3ab22081a7cc7cc85b7c4cb1667b
momo <thangmo_thangmo@hotmail.com>
2015-11-02 12:56:39 +0700

    add file

--- src/main/java/th/co/geniustree/intenship/advisor/config/SecurityConfig.java
+++ src/main/java/th/co/geniustree/intenship/advisor/config/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .headers()
                 .frameOptions()
                 .disable()
-                .and()
+//                .and()
                 .authorizeRequests().anyRequest().authenticated();
     }
 
